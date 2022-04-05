eada19d9b7d8f602806ebd8cb73be36091657bb5
viswa121 <102525806+viswa121@users.noreply.github.com>
2022-04-05 22:25:43 +0530

    Add files via upload

---
+++ EVen numbers.java
@@ -0,0 +1,13 @@
+class Example{
+
+  public static void main(String args[]){
+
+    int arr[]={23,45,56,67,90};
+
+   for(int i=0;i<arr.length;i++){
+     if(arr[i]%2==0) 
+     System.out.print(arr[i]+" ");
+   }
+    
+  }
+}
