1f93eec2326033091ef3a7f319536babe46ba9e3
viswa121 <102525806+viswa121@users.noreply.github.com>
2022-04-05 22:01:16 +0530

    Add files via upload

---
+++ Conditional if.java
@@ -0,0 +1,15 @@
+//main class
+class IfCondition{
+//main method
+public static void main(String[]args){
+//declare a as variable with value 100
+int a=100;
+//start if block
+if(a>=100){//a>100 then only the statement gets executed
+System.out.println("yes he can win the match");
+if(a<100){//a<100 then only the statement gets executed
+System.out.println("no he cannot win the match");
+}
+}
+}
+}
