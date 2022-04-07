ae6e13fa96c898ca1730e8f60435bbb99bca21f1
viswa121 <102525806+viswa121@users.noreply.github.com>
2022-04-07 09:37:07 +0530

    Add files via upload

---
+++ Printing week.java
@@ -0,0 +1,36 @@
+ //WAP to read week number and print weekday
+import java.util.Scanner;
+class Week {
+  public static void main(String args[])
+   {
+      Scanner s=new Scanner(System.in);
+   System.out.println("Enter week number to print weekday");
+
+     int week_num=s.nextInt();
+     if(week_num==1){
+       System.out.println("Sunday");
+     }
+     else if(week_num==2){
+       System.out.println("Monday");
+     }
+     else if(week_num==3){
+       System.out.println("Tuesday");
+     }
+     else if(week_num==4){
+       System.out.println("Wednesday");
+     }
+     else if(week_num==5){
+       System.out.println("Thursday");
+     }
+     else if(week_num==6){
+       System.out.println("Friday");
+     }
+     else if(week_num==7){
+       System.out.println("Saturday");
+     }
+     else{
+       System.out.println("Invalid week number");
+     }
+     
+  }
+}
